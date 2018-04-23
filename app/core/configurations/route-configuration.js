28b52378979c329b14faab8a580fbd0804202cf8
Ravi Upadhyay <raviupadhyay.bkn@gmail.com>
2018-04-23 21:15:14 +0530

    Minor change in route-configuration.js

--- app/core/configurations/route-configuration.js
+++ app/core/configurations/route-configuration.js
@@ -9,23 +9,27 @@
     'use strict';
 
     angular.module('myApp')
-        .config(function ($stateProvider, $locationProvider, $urlRouterProvider, $httpProvider) {
+    .config(function ($stateProvider, $locationProvider, $urlRouterProvider, $httpProvider) {
+    
+    //TODO:to remove '#' from url this is the logic, But having problem when we direct
+    //hit or refresh URL. 
 
-        //Logic to remove hash logic, from the url
-        // $locationProvider.html5Mode({
-        //     enabled: true,
-        //     requireBase: false
-        // });
+    //Logic to remove hash logic, from the url
+    // $locationProvider.html5Mode({
+    //     enabled: true,
+    //     requireBase: false
+    // });
 
-        $urlRouterProvider.otherwise('/hello');
+    $urlRouterProvider.otherwise('/hello');
 
-        var hello = {
-            name: 'hello',
-            url: '/hello',
-            template: '<hello-ui-component></hello-ui-component>'
-        };
+    var hello = {
+        name: 'hello',
+        url: '/hello',
+        template: '<hello-ui-component></hello-ui-component>'
+    };
 
-        $stateProvider.state(hello);
+    $stateProvider.state(hello);
 
-        });
+    });
 })();
+``
